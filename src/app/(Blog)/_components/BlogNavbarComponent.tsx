6e869573743859fcc0cb2d71e1f0d01bb37a3dbf
danielromeo <danielromeo99@gmail.com>
2023-06-28 12:54:53 +0200

    completed the blog side navbar

--- src/app/(Blog)/_components/BlogNavbarComponent.tsx
+++ src/app/(Blog)/_components/BlogNavbarComponent.tsx
@@ -5,13 +5,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from './_styles_components/blogNavbarComponent.module.scss'
+import { Button } from 'react-bootstrap';
+import {BsFillArrowLeftCircleFill} from 'react-icons/bs'
 
 const BlogNavbarComponent = () =>{  
     return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className='justify-content-center'>
+      <Container className={styles.navbarMainContainer}>
+        <Link href='/Blog'><BsFillArrowLeftCircleFill size={50}></BsFillArrowLeftCircleFill></Link>
+        
         <Navbar.Brand className={styles.navbarBrand}><li><Link className={styles.navbarBrandLink} href='/home'>IVY</Link></li></Navbar.Brand>
-       
+        <div></div>
       </Container>
     </Navbar>
   );
