052dfc1cbfd3c62f5abed9eadd41a54f76b41370
danielromeo <danielromeo99@gmail.com>
2024-07-09 16:34:10 +0200

    added a banner for the commerce home page

--- src/app/(Commerce)/commerce/_commerceComponent.tsx
+++ src/app/(Commerce)/commerce/_commerceComponent.tsx
@@ -1,16 +1,43 @@
-"use client";
+'use client'
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import styles from '../_styles/commerceComponent.module.scss';
 
-import { Container } from "react-bootstrap";
-
-const CommerceComponent = () => {
-    return (
-		<div>
-			<Container>
-				<h1> Commerce home page</h1>
-			</Container>
-		</div>
-      
-    )
-  }
+const CommerceComponent: React.FC = () => {
+  return (
+    <div className={styles.landingPage}>
+      <Carousel controls={false} indicators={false}>
+        <Carousel.Item>
+          <div className={styles.carouselImage} style={{backgroundImage: 'url("/nikeBanner.webp")'}}>
+            <div className={styles.carouselCaption}>
+              <h1>STREET FASHION REDEFINED</h1>
+              <button className={styles.shopButton}>SHOP BRANDS</button>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={styles.carouselImage} style={{backgroundImage: 'url("/nikeBanner.webp")'}}>
+            <div className={styles.carouselCaption}>
+              <h1>URBAN STYLE UNLEASHED</h1>
+              <button className={styles.shopButton}>EXPLORE NOW</button>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className={styles.carouselImage} style={{backgroundImage: 'url("/nikeBanner.webp")'}}>
+            <div className={styles.carouselCaption}>
+              <h1>BOLD LOOKS, BOLD STATEMENTS</h1>
+              <button className={styles.shopButton}>DISCOVER MORE</button>
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+      <div className={styles.newArrivals}>
+        <h2>NEW ARRIVALS</h2>
+        {/* Add new arrivals content here */}
+      </div>
+    </div>
+  );
+};
 
 export default CommerceComponent;
