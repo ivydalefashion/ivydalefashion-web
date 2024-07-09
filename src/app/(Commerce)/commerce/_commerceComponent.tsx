791f35d6038cdbb0a364d0357f91d91fd01432f7
danielromeo <danielromeo99@gmail.com>
2024-07-09 16:47:25 +0200

    added the new arrivals section

--- src/app/(Commerce)/commerce/_commerceComponent.tsx
+++ src/app/(Commerce)/commerce/_commerceComponent.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container, Row, Col } from 'react-bootstrap';
+import { motion } from 'framer-motion';
 import styles from '../_styles/commerceComponent.module.scss';
 
 const CommerceComponent: React.FC = () => {
@@ -32,10 +33,35 @@ const CommerceComponent: React.FC = () => {
           </div>
         </Carousel.Item>
       </Carousel>
-      <div className={styles.newArrivals}>
+
+
+      <Container className={styles.newArrivals}>
         <h2>NEW ARRIVALS</h2>
-        {/* Add new arrivals content here */}
-      </div>
+        <Carousel indicators={false}>
+          {[0, 1].map((_, idx) => (
+            <Carousel.Item key={idx}>
+              <Row>
+                {[1, 2, 3, 4].map((itemIdx) => (
+                  <Col key={itemIdx} xs={6} md={3}>
+                    <motion.div
+                      whileHover={{ scale: 1.05 }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <img
+                        className="d-block w-100"
+                        src={`https://s3-alpha-sig.figma.com/img/76fe/2c36/519ab52c5174bbd57b6b632a04721578?Expires=1721606400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WRO1pv~ZLFYmxnayA61jG7KYgVc5X34Xy9NoV-qepZbjiiWdFboVEsyBnAJ2J2eYWYBWcR3lVQqLTvpXdrkvp-P~8GHIhmcctrVSA6b9wiI0ICPo3zUOSXPmmMzz2jAwoPwqy-6c0Eh8a6MI37N40QD-pJhgC6hNaZ6mP307zDreJoMjgqhl0IPffQTAJF3ae5t8QCvsbUE5VOi~MGe2TcW6sS1WLqX7gVI6NaplMBziKlFyiYwrQDaQmbr2B6GM-f-T8tz9J2G8zAozPUmcBlhGIi0nA1cYqVMgYJ0MeBk5xDEGTjcEwoHIiyPCjyyrtZaUR2~UTHWYJQSo9TwW7g__`}
+                        alt={`New Arrival ${idx * 2 + itemIdx}`}
+                      />
+                      <h3>ITEM NAME</h3>
+                      <p>R350</p>
+                    </motion.div>
+                  </Col>
+                ))}
+              </Row>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Container>
     </div>
   );
 };
