37285c8c7c67b61fef7045e704a531920ae431b6
danielromeo <danielromeo99@gmail.com>
2024-07-14 15:06:52 +0200

    replaced formgroup with inputgroup in signup

--- src/app/(Commerce)/signin/_signinComponent.tsx
+++ src/app/(Commerce)/signin/_signinComponent.tsx
@@ -1,63 +1,66 @@
 'use client';
 
 import Link from 'next/link';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container , Row, Col, InputGroup} from 'react-bootstrap';
 import styles from '../_styles/signinComponent.module.scss';
 
 const SigninComponent = () => {
 	return (
-		<div className={`container ${styles.mainContainer} `}>
-			<div className={`row`}>
-				<h2 className={` ${styles.primaryLabel}`}>Log in.</h2>
-				<label className={`${styles.secondaryLabel}`}>
-					Please enter your email and password
-				</label>
-			</div>
+		<div>
+			<Container className={` ${styles.mainContainer} `}>
+				<Row >
+					<h2 className={` ${styles.primaryLabel}`}>Log in.</h2>
+					<label className={`${styles.secondaryLabel}`}>
+						Please enter your email and password
+					</label>
+				</Row>
 
-			<div className={`row`}>
-				<form className={` ${styles.mainForm}`}>
-					<Form.Group>
-						<input
-							className={`form-control shadow-none ${styles.input} ${styles.emailInput}`}
-							type="email"
-							name="email"
-							id=""
-							placeholder="email address"
-						/>
-					</Form.Group>
+				<Row>
+					<form className={` ${styles.mainForm}`}>
 
-					<Form.Group>
-						<div className="input-group">
+						{/* Email input field */}
+						<Form.Group>
 							<input
-								className={`form-control shadow-none ${styles.input} ${styles.passwordInput}`}
-								type="password"
-								name="password"
+								className={`form-control shadow-none ${styles.input} ${styles.emailInput}`}
+								type="email"
+								name="email"
 								id=""
-								placeholder="password"
+								placeholder="email address"
 							/>
-							<button
-								type="button"
-								className={`input-group-text ${styles.forgotPasswordButton}`}
-								id="basic-addon2"
-							>
+						</Form.Group>
+
+						{/* Password input field */}
+						<InputGroup className="mb-3">
+							<Form.Control
+								className={`form-control shadow-none ${styles.input} ${styles.passwordInput}`}
+								name="password"
+								type="password"
+								placeholder="Enter password"
+								aria-label="Password"
+								aria-describedby="basic-addon2"
+							/>
+							<Button variant="outline-secondary" 
+									className={`input-group-text ${styles.forgotPasswordButton}`}
+									id="button-addon2">
 								forgot password?
-							</button>
-						</div>
-					</Form.Group>
+							</Button>
+						</InputGroup>
 
-					<Button type="submit" className={`${styles.loginButton}`}>
-						LOGIN
-					</Button>
+						{/* Login button */}
+						<Button type="submit" className={`${styles.loginButton}`}>
+							LOGIN
+						</Button>
 
-					<br />
-
-					<Link href="signup">
-						<p className={styles.createAccountLink}>
-							New customer? <span>Create an account</span>
-						</p>
-					</Link>
-				</form>
-			</div>
+						<br />
+						
+						<Link href="signup">
+							<p className={styles.createAccountLink}>
+								New customer? <span>Create an account</span>
+							</p>
+						</Link>
+					</form>
+				</Row>
+			</Container>
 		</div>
 	);
 };
