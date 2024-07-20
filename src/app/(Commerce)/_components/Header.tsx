1824e43818af1ffeb632464bb35e9f3a9a6ebb28
danielromeo <danielromeo99@gmail.com>
2024-07-20 11:36:54 +0200

    changed the tooltip and did progress on the navbar

--- src/app/(Commerce)/_components/Header.tsx
+++ src/app/(Commerce)/_components/Header.tsx
@@ -1,12 +1,39 @@
 'use client';
 
-import React from 'react';
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Navbar, Container, Nav, NavDropdown, InputGroup, Button, Form } from 'react-bootstrap';
 import Link from 'next/link';
 import styles from './_styles_components/Header.module.scss';
 import { FaSearch, FaUserAlt, FaShoppingCart, FaHeart } from 'react-icons/fa';
 
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// Schema:
+const schema = yup.object().shape({
+	search: yup.string().required('Search cannot be empty.'),
+});
+
+
+
 const MainNavbar = () => {
+	const router = useRouter();
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({ resolver: yupResolver(schema) });
+
+	const onSubmit = (data: any) => {
+		console.log(data);
+		// Handle form submission here
+
+		router.push('/commerce');
+	};
+
 	return (
 		<div>
 			<Navbar className={`${styles.navBar}`} bg="light" expand="lg">
@@ -26,42 +53,49 @@ const MainNavbar = () => {
 						</Nav>
 
 						<Nav className="me-auto">
-							<div className="input-group">
-								<input
-									className={`form-control shadow-none ${styles.searchBar}`}
-									type="text"
-									placeholder="Search brands/categories/products"
-									aria-label="Search brands/categories/products"
-									aria-describedby="basic-addon2"
-								/>
-								<span
-									className={`input-group-text ${styles.searchButtonSpan}`}
-									id="basic-addon2"
-								>
-									<FaSearch></FaSearch>
-								</span>
-							</div>
+							<Form onSubmit={handleSubmit(onSubmit)}>
+								<InputGroup className={`${styles.inputGroup}`}>
+									<Form.Control
+										className={`form-control shadow-none ${errors.search ? styles.errorInput : ''} ${styles.input}`}
+										{...register('search')}
+										type="text"
+										placeholder="Search brands/..."
+										aria-label="search"
+										aria-describedby="basic-addon2"
+									/>
+									{/* <Link href="/search"> */}
+										<Button
+											variant="outline-secondary"
+											className={`input-group-text ${styles.searchButton}`}
+											id="button-addon2"
+											type='submit'
+										>
+											Search
+										</Button>
+									{/* </Link> */}
+								</InputGroup>
+							</Form>
 						</Nav>
 
 						<Nav className={styles.navNavigationIcons}>
 							<Nav.Link className={styles.navNavigationIconsNavLink}>
 								<section>
 									<Link href="/wishlist">
-										<FaHeart></FaHeart>
+										<FaHeart className={styles.icon} data-toggle="tooltip" data-placement="top" title="Wishlist"></FaHeart>
 									</Link>
 								</section>
 							</Nav.Link>
 							<Nav.Link className={styles.navNavigationIconsNavLink} eventKey={2}>
 								<section>
 									<Link href="/profile">
-										<FaUserAlt></FaUserAlt>
+										<FaUserAlt className={styles.icon} data-toggle="tooltip" data-placement="top" title="Profile"></FaUserAlt>
 									</Link>
 								</section>
 							</Nav.Link>
 							<Nav.Link className={styles.navNavigationIconsNavLink} eventKey={3}>
 								<section>
 									<Link href="/cart">
-										<FaShoppingCart></FaShoppingCart>
+										<FaShoppingCart className={styles.icon} data-toggle="tooltip" data-placement="top" title="Cart"></FaShoppingCart>
 									</Link>
 								</section>
 							</Nav.Link>
