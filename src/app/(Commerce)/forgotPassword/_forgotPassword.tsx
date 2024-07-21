3b4ff114c4594066c0a27c4472283a09718383e7
danielromeo <danielromeo99@gmail.com>
2024-07-21 06:55:56 +0200

    added forgot password page

---
+++ src/app/(Commerce)/forgotPassword/_forgotPassword.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import styles from '../_styles/buyComponent.module.scss';
+import AlertDismissible from '../_components/DismissableAlert';
+import Link from 'next/link';
+
+const ForgotPasswordComponent = () => {
+	const [quantity, setQuantity] = useState(1);
+
+	return (
+		<div className={styles.main}>
+            <Container>
+            <h5>Forogt your password? </h5>
+            </Container>
+			
+		</div>
+	);
+};
+
+export default ForgotPasswordComponent;
--- src/app/(Commerce)/forgotPassword/page.tsx
+++ src/app/(Commerce)/forgotPassword/page.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Metadata } from 'next';
+import MainNavbar from '../_components/Header';
+import Footer from '../_components/Footer';
+import ForgotPasswordComponent from './_forgotPassword';
 
 export const metadata: Metadata = {
-	title: 'Signin to IvydaleFashion',
-	description: 'Sign in or Login here to access your Ivydalefashion account.',
+	title: '',
+	description: '',
 };
 
 const ForgotPasswordPage = () => {
-	return <div>forgot password? enter your email here...</div>;
+	return (
+		<div>
+			<MainNavbar></MainNavbar>
+			<ForgotPasswordComponent></ForgotPasswordComponent>
+			<Footer></Footer>
+		</div>
+		
+	)
 };
 
 export default ForgotPasswordPage;
