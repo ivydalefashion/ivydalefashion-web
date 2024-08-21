0967f6360c98b639d000936c9a684143adc4d4f7
danielromeo <danielromeo99@gmail.com>
2024-08-21 14:18:25 +0200

    added the no orders component and styling

--- frontend/src/app/(Commerce)/profile/ordersComponent.tsx
+++ frontend/src/app/(Commerce)/profile/ordersComponent.tsx
@@ -3,17 +3,43 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { Form, Button, Container, Row, Col, InputGroup } from 'react-bootstrap';
+import styles from '../_styles/ordersComponent.module.scss'
 
+// There was no need to use font awesome, could have used React icons. You will fix when you have time
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icons'
+
+
+// 
 const OrdersComponent = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [orders, setOrders] = useState<any>([]);
 
 	return (
-		<div>
-			<Container>
-				<Row>
-					<h1>Orders</h1>
-				</Row>
-			</Container>
+		<div className={styles.main}>
+			{
+				orders.length > 0 ? (
+					<Container className={styles.mainContainer}>
+						<Row className={styles.mainRow}>
+							<h1>Orders   1 2 3</h1>
+						</Row>
+					</Container>
+				): (
+					<Container className={styles.mainContainer}>
+						<Row className={styles.mainRow}>
+							<Col className={styles.col}>
+								<FontAwesomeIcon className={styles.icon} icon={faBoxOpen} size="4x" />
+								<p>You don't have any orders yet.</p>
+								<hr />
+								<Button className={styles.goToShoppingButton}>Go to shopping</Button>
+							</Col>
+							
+						</Row>
+					</Container>
+				)
+			}
+			
 		</div>
 	);
 };
