ddf765e96d10f1797aa4ca4b180ff9fa651f97d0
danielromeo <danielromeo99@gmail.com>
2024-09-26 10:51:58 +0200

    fixed buy page improt

--- frontend/src/app/(Commerce)/buy/[buyid]/_buyComponent.tsx
+++ frontend/src/app/(Commerce)/buy/[buyid]/_buyComponent.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import styles from '../_styles/buyComponent.module.scss';
+import styles from '../../_styles/buyComponent.module.scss';
 import AlertDismissible from '../../_components/DismissableAlert';
 import Link from 'next/link';
 
