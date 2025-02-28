cb0dae6325f7bb7b2fe6c5343bad9d5594405d83
danielromeo <danielromeo99@gmail.com>
2025-02-28 20:55:27 +0200

    minor update

--- frontend/src/app/(Commerce)/explore/exploreComponent.tsx
+++ frontend/src/app/(Commerce)/explore/exploreComponent.tsx
@@ -33,33 +33,41 @@ const BrandComponent = () => {
 					<Col className={styles.imagesCol} lg={10} md={10} sm={12}>
 						{/* Card with image */}
 
-						{[1, 2, 3, 4, 5, 6].map((item: any) => (
-							<Col className={``} lg={3} md={3} sm={12}>
-								<Card className={styles.card} style={{}}>
-									<ResponsiveImage
-										src={'/orangeHoodie.png'}
-										alt={`New Arrival `}
-										height={80}
-										width={80}
-									></ResponsiveImage>
+						<Row className={styles.imagesColTitleRow}>
+							<h1>UPDATE</h1>
+						</Row>
 
-									<Card.Body className={` ${styles.cardBody}`}>
-										<Card.Title className={`${styles.titleText}`}>
-											Manchester united jersey 2022 Away
-										</Card.Title>
-										<Card.Text className={`${styles.priceText}`}>
-											R600
-										</Card.Text>
-										<Button
-											className={`${styles.addToCartButton}`}
-											variant="success"
-										>
-											Add to cart
-										</Button>
-									</Card.Body>
-								</Card>
-							</Col>
-						))}
+						<Row>
+							{[1, 2, 3, 4, 5, 6].map((item: any) => (
+								<Col className={``} lg={2} md={2} sm={12}>
+									<Card className={styles.card} style={{}}>
+										<ResponsiveImage
+											src={'/orangeHoodie.png'}
+											alt={`New Arrival `}
+											height={80}
+											width={80}
+										></ResponsiveImage>
+
+										<Card.Body className={` ${styles.cardBody}`}>
+											<Card.Title className={`${styles.titleText}`}>
+												Manchester united jersey 2022 Away
+											</Card.Title>
+											<Card.Text className={`${styles.priceText}`}>
+												R600
+											</Card.Text>
+											<Button
+												className={`${styles.addToCartButton}`}
+												variant="success"
+											>
+												Add to cart
+											</Button>
+										</Card.Body>
+									</Card>
+								</Col>
+							))}
+						</Row>
+
+						
 					</Col>
 				</Row>
 			</Container>
